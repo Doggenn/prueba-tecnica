2f27409b6b49c185cd8469171184e07fcac27abe
Doggenn <19arg09@gmail.com>
2024-04-03 16:01:15 +0200

    paso 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import './App.css'
+import Cards from './components/Cards'
+import Card from './components/Card'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
     <>
-      
+      <Header></Header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Cards></Cards>} />
+          <Route path='/:id' element={<Card></Card>} />
+        </Routes>
+      </Router>
     </>
   )
 }
